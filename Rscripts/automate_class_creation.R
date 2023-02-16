046697c68301c324a477da34a019403b64abba6c
Yves Amevoin <amevoina@yahoo.fr>
2023-02-16 13:06:40 +0100

    prepared the setup folder for further developments

--- Rscripts/automate_class_creation.R
+++ Rscripts/automate_class_creation.R
@@ -1,4 +1,36 @@
 
+
+# create a function to save the current setup
+
+update_setup  <- function(update_stable = 0) {
+    #move previous version of my designer
+   file.copy(from = "./setup_aky.xlsb",
+             to = "./Rscripts/", overwrite = TRUE)
+    # move back and overwrite
+    file.copy(from = "./Rscripts/setup_aky.xlsb",
+             to = "./setup_dev.xlsb", overwrite = TRUE)
+    # update the stable version if needed
+    if (update_stable == 1) {
+       # previous stable version
+         file.copy(from = "./setup.xlsb",
+             to = "./Rscripts/setup_prev.xlsb", overwrite = TRUE)
+       # update the new stable version
+         file.copy(from = "./Rscripts/setup_aky.xlsb",
+             to = "./setup.xlsb", overwrite = TRUE)
+    }
+    # revert back previous stable designer due to corrupt files.
+    if (update_stable == 2) {
+         file.copy(from = "./Rscripts/setup_prev.xlsb",
+             to = "./setup.xlsb", overwrite = TRUE)
+         file.copy(from = "./Rscripts/setup_prev.xlsb",
+                 to = "./setup_dev.xlsb", overwrite = TRUE)
+         file.copy(from = "./Rscripts/setup_prev.xlsb",
+                 to = "./setup_aky.xlsb", overwrite = TRUE)
+    }
+}
+
+
+
 # create en empty file for creating an interface for each of the class
 
 class_header  <- function(class_name, description = "", module_description = "",
--- Rscripts/automation_script.R
+++ Rscripts/automation_script.R
@@ -2,3 +2,7 @@
 source("./Rscripts/automate_class_creation.R")
 
 
+update_setup()
+
+# update by adding the stable
+#update_setup(update_stable = 1)
