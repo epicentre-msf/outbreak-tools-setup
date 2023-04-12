cecce4aa7f799e679108c653a3af79ac23e82895
Yves Amevoin <amevoina@yahoo.fr>
2023-04-12 17:18:56 +0200

    Preparing the setup for a new version

--- Rscripts/update_setup.R
+++ Rscripts/update_setup.R
@@ -36,8 +36,8 @@ update_setup  <- function(update_status = 0) {
 }
 
 #update the dev file in bin
-#update_setup(update_status = 0) #nolint
+update_setup(update_status = 0) #nolint
 #update the file on root setup
-#update_setup(update_status = 1) #nolint
+update_setup(update_status = 1) #nolint
 #replace the _aky setup by the _mock setup
 update_setup(update_status = 3) #nolint
